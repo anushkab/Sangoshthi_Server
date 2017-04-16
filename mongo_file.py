451e41b6af8b383aec4cb8e49223ff9bece08692
garimachhikara <garima15014@iiitd.ac.in>
2017-04-17 03:09:46 +0530

    change terms

--- mongo_file.py
+++ mongo_file.py
@@ -134,7 +134,7 @@ def insert_delete_trailer_stats(data):
 def insert_call_broadcaster_stats(data):
     with app.app_context():
         entries = mongo.db.call_broadcaster_stats
-		print('hii' , data)
+	print('hii' , data)
         status = entries.insert(data)
         print(data)
         return status
